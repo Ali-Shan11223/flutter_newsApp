115f9b4b9d8323a80009af9e544346ae6e490da2
Ali-Shan11223 <jonah2114@gmail.com>
2023-10-25 02:03:03 +0500

    first commit

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/images/splash_pic.jpg',
+            height: size.height * 0.3,
+            fit: BoxFit.contain,
+          ),
+          SizedBox(
+            height: size.height * 0.04,
+          ),
+          Text(
+            'TOP HEADLINES',
+            style: GoogleFonts.antic(
+                letterSpacing: 0.6,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey.shade700),
+          ),
+          SizedBox(
+            height: size.height * 0.04,
+          ),
+          const SpinKitChasingDots(
+            color: Colors.blue,
+          )
+        ],
+      ),
+    );
+  }
+}
